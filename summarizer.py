ddd9f43111568fd43cfda611821e2ad5fb0103db
gf_yokota <153173194+yokonatsu@users.noreply.github.com>
2024-10-01 16:50:06 +0900

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -194,7 +194,7 @@ def runner():
             text = summarize("\n".join(splitted_messages), LANGUAGE)
             result_text.append(text)
 
-    title = (f"{start_time.strftime('%Y-%m-%d')} public channels summary\n\n")
+    title = (f"{start_time.strftime('%Y-%m-%d')} の日報")
 
     if DEBUG:
         print("\n".join(result_text))
